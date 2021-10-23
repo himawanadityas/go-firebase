e3a4f94646cfeee0d5cc0e8ef8ab50cbb5dabede
Himawan Adityas <adityashimawan@yahoo.com>
2021-10-23 13:01:06 +0700

    up

--- config/config.go
+++ config/config.go
@@ -1,2 +1,26 @@
 package config
 
+import (
+	"context"
+	"fmt"
+	"log"
+
+	firebase "firebase.google.com/go"
+	"google.golang.org/api/option"
+)
+
+func Connection(){
+	option := option.WithCredentialFiles("firebaseKey.json")
+	ctx := context.Background()
+	conf := &firebase.config{DatabaseURL:"https://data-118cf.firebaseio.com"}
+	apps, err := firebase.NewApp(ctx, conf, option)
+	if err != nil {
+		fmt.Println(err)
+	}
+	initDB, err := apps.Database(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	db := initDB.NewRef("my-apps")
+	return db, apps
+}
